3c0dc5de44280b3f31de6830bdf07190329751ab
dzikru07 <dzikru07@gmail.com>
2023-11-12 01:53:33 +0700

    Initial Commit

---
+++ youtube_clone_ios/youtube_clone_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_clone_iosApp.swift
+//  youtube_clone_ios
+//
+//  Created by dzikru arya on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct youtube_clone_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
